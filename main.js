3ee7a892f7fe5bd09362d8faaf1a3ea5b3e570e2
Alessandro <alessandro.mancinelli89@gmail.com>
2020-12-01 17:22:06 +0100

    Filter implemented

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 /* 
 Facciamo una chiamata ajax all'api di boolean al seguente indirizzo.
 L'api ci restituirà decina di dischi musicali che dovremo mostrare all'utente utilizzando Vue.
+Creare una select con i seguenti generi: pop, rock, metal e jazz.
+Mostra elementi in base al genere
 */
 
 const app = new Vue({
 	el:"#root",
 	data: {
+		selected: '',
 		albums: [],
 	},
 	mounted() {
+		//Chiamata API per recuperare i dati necessari che vengono salvati in albums[]
 		axios.get('https://flynn.boolean.careers/exercises/api/array/music')
 		.then(result => {
  			let {response} = result.data;
@@ -16,5 +20,19 @@ const app = new Vue({
 				this.albums.push(response[i]);
 			} 
 		})
-	}
+	},
+	computed:{ 
+		//Imposta filtro per genere
+		filteredMusic(){
+			if (this.selected == ''){
+				return this.albums
+			} else {
+				return this.albums.filter(element=>{
+					if (this.selected == element.genre){
+						return element;
+					}
+				})
+			}
+		}
+	},
 })
